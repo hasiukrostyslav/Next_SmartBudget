95d423e65aae3fe3088c017813d37e0fe6e0d0d9
Rostyslav Hasiuk <hasiukrostyslav@gmail.com>
2025-09-21 14:57:08 +0300

    Change Logo svg component to svg files

--- src/_components/Logo.tsx
+++ src/_components/Logo.tsx
@@ -1,5 +1,16 @@
-import LogoText from './LogoText';
+'use client';
+
+import Image from 'next/image';
+import useTheme from '@/_hooks/useTheme';
 
 export default function Logo() {
-  return <LogoText />;
+  const { theme } = useTheme();
+  return (
+    <Image
+      src={theme === 'light' ? '/logo-dark.svg' : '/logo-light.svg'}
+      alt='Logo'
+      width={200}
+      height={100}
+    />
+  );
 }
