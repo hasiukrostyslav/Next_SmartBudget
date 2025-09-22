ce9db17c1a3a5ae4b1c4dfa80e4bfd2ca0b7d976
Rostyslav Hasiuk <hasiukrostyslav@gmail.com>
2025-09-22 21:25:46 +0300

    Add Images optimization

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -24,7 +24,14 @@ export default function RootLayout({
       <body className={`${roboto.className}`}>
         <main className='flex h-screen w-screen text-slate-900 dark:text-slate-300 dark:bg-slate-900'>
           <div className='relative w-7/12'>
-            <Image src='/background.jpg' fill alt='background image' />
+            <Image
+              src='/background.jpg'
+              alt='background image'
+              sizes='(max-width: 768px) 50vw, (max-width: 1200px) 70vw, 100vw'
+              quality={100}
+              fill
+              priority
+            />
           </div>
           <ThemeProvider>{children}</ThemeProvider>
         </main>
