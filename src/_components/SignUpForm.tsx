01a091b2ac7b1a9439e9c9356eeb1998622d82d1
Rostyslav Hasiuk <hasiukrostyslav@gmail.com>
2025-09-28 21:05:50 +0300

    Add simple version of login and signup functions

--- src/_components/SignUpForm.tsx
+++ src/_components/SignUpForm.tsx
@@ -1,9 +1,14 @@
+import { signUp } from '@/_lib/userActions';
 import Button from './Button';
 import Input from './Input';
 
 export default function SignUpForm() {
   return (
-    <form autoComplete='off' className='flex flex-col w-full gap-2 mt-6'>
+    <form
+      action={signUp}
+      autoComplete='off'
+      className='flex flex-col w-full gap-2 mt-6'
+    >
       <Input label='Name' name='name' />
       <Input label='Email address' name='email' />
       <Input label='Password' name='password' isPassword />
