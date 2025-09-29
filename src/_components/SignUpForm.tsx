cc0b35125f7a87a915a5714657e2788b4c143df6
Rostyslav <72873964+hasiukrostyslav@users.noreply.github.com>
2025-09-29 16:40:15 +0300

    Revert "Merge all from dev"

--- src/_components/SignUpForm.tsx
+++ src/_components/SignUpForm.tsx
@@ -1,14 +1,9 @@
-import { signUp } from '@/_lib/userActions';
 import Button from './Button';
 import Input from './Input';
 
 export default function SignUpForm() {
   return (
-    <form
-      action={signUp}
-      autoComplete='off'
-      className='flex flex-col w-full gap-2 mt-6'
-    >
+    <form autoComplete='off' className='flex flex-col w-full gap-2 mt-6'>
       <Input label='Name' name='name' />
       <Input label='Email address' name='email' />
       <Input label='Password' name='password' isPassword />
