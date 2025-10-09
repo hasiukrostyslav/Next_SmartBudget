7aed7b16e140c9d722fb03fd7c04ef2f5293503c
Rostyslav Hasiuk <hasiukrostyslav@gmail.com>
2025-10-09 18:12:42 +0300

    Check email for unique and change reture object from signup action

--- src/_lib/userActions.ts
+++ src/_lib/userActions.ts
@@ -4,25 +4,47 @@ import z from 'zod';
 import { redirect } from 'next/navigation';
 import { prisma } from './db';
 import { SignInSchema, SignUpSchema } from './schema';
+import { cookies } from 'next/headers';
 
 type SignUpFormData = z.infer<typeof SignUpSchema>;
 type SignInFormData = z.infer<typeof SignInSchema>;
 
 export async function signUp(formData: SignUpFormData) {
+  // Form data validation
   const validatedFields = SignUpSchema.safeParse(formData);
 
   if (!validatedFields.success) {
     return {
-      error: z.treeifyError(validatedFields.error).properties,
+      errors: z.flattenError(validatedFields.error).fieldErrors,
     };
   }
 
+  // Checking if account with provided email exist
+  const existUser = await prisma.users.findUnique({
+    where: { email: validatedFields.data.email },
+    select: { email: true },
+  });
+
+  if (existUser)
+    return {
+      errors: { email: ['An account with this email already exists.'] },
+    };
+
+  // Create new user
   await prisma.users.create({
     data: {
       user_name: validatedFields.data.name,
       email: validatedFields.data.email,
       password: validatedFields.data.password,
     },
+  });
+
+  // Set cookie for Toast component
+  const cookieStore = cookies();
+  (await cookieStore).set({
+    name: 'signup_success',
+    value: '1',
+    maxAge: 1,
   });
 
   redirect('/dashboard');
