c22149fb95630b5f0c1fa13fcfff458bfdf30491
Rostyslav Hasiuk <hasiukrostyslav@gmail.com>
2025-10-30 19:18:34 +0200

    Add Dashbord layout structure

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,19 +1,3 @@
-import { auth, signOut } from '@/auth/auth';
-
 export default async function page() {
-  const session = await auth();
-
-  return (
-    <div>
-      <form
-        action={async () => {
-          'use server';
-          await signOut({ redirectTo: '/auth/login' });
-        }}
-      >
-        <h1 className="text-3xl">Hello {session?.user?.name}</h1>
-        <button type="submit">Sign Out</button>
-      </form>
-    </div>
-  );
+  return <section className="col-start-3 -col-end-1"></section>;
 }
---
+++ src/app/(protected)/layout.tsx
@@ -0,0 +1,18 @@
+import Footer from '@/components/layouts/Footer';
+import Header from '@/components/layouts/Header';
+import Sidebar from '@/components/layouts/Sidebar';
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <section className="grid h-screen grid-cols-14 grid-rows-[auto_1fr_auto]">
+      <Header />
+      <Sidebar />
+      {children}
+      <Footer />
+    </section>
+  );
+}
