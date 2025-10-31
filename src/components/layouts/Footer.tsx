2f015ef038d2274ebbdc3dc0ee5d769b27fd32d7
Rostyslav Hasiuk <hasiukrostyslav@gmail.com>
2025-10-31 12:43:46 +0200

    Change Footer padding

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <footer className="bg-stale-100 col-start-3 -col-end-1 flex justify-center gap-20 border-t-2 border-blue-400 px-16 py-6">
+    <footer className="bg-stale-100 col-start-3 -col-end-1 flex justify-center gap-20 px-16 py-4">
       <p className="text-xs">Copyright &copy; 2025 SmartBudget</p>
       <ul className="flex gap-4 text-xs">
         <li className="hover:text-slate-500">
