efd223ffa6612f18a97824a696f5e7d3dca2d731
Rostyslav Hasiuk <hasiukrostyslav@gmail.com>
2025-10-31 09:11:18 +0200

    Style footer

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,3 +1,20 @@
+import Link from 'next/link';
+
 export default function Footer() {
-  return <footer className="col-start-3 -col-end-1 bg-blue-300">Footer</footer>;
+  return (
+    <footer className="bg-stale-100 col-start-3 -col-end-1 flex justify-center gap-20 border-t-2 border-blue-400 px-16 py-6">
+      <p className="text-xs">Copyright &copy; 2025 SmartBudget</p>
+      <ul className="flex gap-4 text-xs">
+        <li className="hover:text-slate-500">
+          <Link href="#">Privacy Policy</Link>
+        </li>
+        <li className="hover:text-slate-500">
+          <Link href="#">Term and Condition</Link>
+        </li>
+        <li className="hover:text-slate-500">
+          <Link href="#">Contact</Link>
+        </li>
+      </ul>
+    </footer>
+  );
 }
