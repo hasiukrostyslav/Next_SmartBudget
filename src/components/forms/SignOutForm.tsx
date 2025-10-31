a9427f0bd20877282fb45b2c4f82bdb4fd267f3d
Rostyslav Hasiuk <hasiukrostyslav@gmail.com>
2025-10-31 12:41:43 +0200

    Style Header section

---
+++ src/components/forms/SignOutForm.tsx
@@ -0,0 +1,14 @@
+import ButtonIcon from '../ui/ButtonIcon';
+
+export default function SignOutForm() {
+  return (
+    <form>
+      <ButtonIcon
+        size={18}
+        iconName="log-out"
+        shape="square"
+        variant="outline"
+      />
+    </form>
+  );
+}
