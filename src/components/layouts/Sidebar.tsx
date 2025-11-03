6953782b96d056a267843549eb8f8e5e590b34fa
Rostyslav Hasiuk <hasiukrostyslav@gmail.com>
2025-11-03 21:26:24 +0200

    Fix: Sidebar bg-color flash

--- src/components/layouts/Sidebar.tsx
+++ src/components/layouts/Sidebar.tsx
@@ -13,7 +13,8 @@ export default function Sidebar() {
     <aside
       className={clsx(
         'relative row-span-full flex flex-col overflow-hidden px-4 py-2.5',
-        'border-r-2 border-blue-400 bg-slate-100 transition-all duration-1000 ease-in-out dark:bg-slate-800',
+        'border-r-2 border-blue-400 bg-slate-100 dark:bg-slate-800',
+        'transition-[width] duration-1000 ease-in-out',
         isCollapsed ? 'w-18' : 'w-58',
       )}
     >
