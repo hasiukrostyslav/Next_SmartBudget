7cf05609bf76d61a143b44b4c552963aeea95a18
Rostyslav Hasiuk <hasiukrostyslav@gmail.com>
2025-11-06 13:08:36 +0200

    Style first part of the pagination

--- src/app/(protected)/dashboard/transactions/page.tsx
+++ src/app/(protected)/dashboard/transactions/page.tsx
@@ -1,13 +1,13 @@
 import TransactionsFilters from '@/components/ui/TransactionsFilters';
 import TransactionsList from '@/components/ui/TransactionsList';
-import Pagination from '@/components/ui/Pagination';
+import PaginationTable from '@/components/ui/PaginationTable';
 
 export default function TransactionsPage() {
   return (
     <section className="grid h-full grid-rows-[auto_1fr_auto] gap-4">
       <TransactionsFilters />
       <TransactionsList />
-      <Pagination />
+      <PaginationTable />
     </section>
   );
 }
