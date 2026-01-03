a2a65965ddfbd1951bf195269c1b040ff141ac9d
Rostyslav Hasiuk <hasiukrostyslav@gmail.com>
2026-01-03 18:03:23 +0200

    chore: update prisma to 7.2.0, @prisma/client to 7.2.0 and change prisma config

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -1,7 +1,15 @@
 import { PrismaClient } from '../../../generated/client';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const db = globalForPrisma.prisma || new PrismaClient();
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+export const db = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
