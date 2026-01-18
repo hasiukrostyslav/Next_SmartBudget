edb9af3c69d6280ca8185d480cb85c3bf0b51115
Rostyslav Hasiuk <hasiukrostyslav@gmail.com>
2026-01-18 16:29:34 +0200

    Make Year in the footer dynamic

---
+++ src/components/ui/Copyright.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function Copyright() {
+  const year = new Date().getFullYear();
+
+  return (
+    <p className="self-end">Copyright &copy; {year} SmartBudget</p>
+  )
+}
